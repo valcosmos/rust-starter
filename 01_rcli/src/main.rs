4329d3ff0f500fc106358bb90386157cf77e26e6
Cupid Valentine <westerngod@126.com>
2024-03-17 23:55:30 +0800

    feat: try read a csv file

---
+++ 01_rcli/src/main.rs
@@ -0,0 +1,33 @@
+use anyhow::{Ok, Result};
+use serde::Deserialize;
+use std::fs::File;
+
+#[derive(Debug, Deserialize)]
+struct Player {
+    #[serde(rename="ID")]
+    id: u8,
+    #[serde(rename="Name")]
+    name: String,
+    #[serde(rename="Age")]
+    age: u8,
+    #[serde(rename="Country")]
+    country: String,
+}
+
+fn main() -> Result<()> {
+    // let content = read_file("assets/demo.csv")?;
+    let file = File::open("assets/demo.csv")?;
+    let mut reader = csv::Reader::from_reader(file);
+    for result in reader.deserialize() {
+        let player: Player = result?;
+        // println!("{:?}", player.ID);
+        println!("Id:{}, Name: {}, Age: {}, Country: {}", player.id, player.name, player.age, player.country)
+    }
+    Ok(())
+}
+
+// fn read_file(file_path: &str) -> Result<String> {
+// //    let content =  fs::read_to_string(file_path).expect("Error reading file");
+//    let content =  fs::read_to_string(file_path)?;
+//    Ok(content)
+// }
