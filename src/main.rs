b8fe44c11979f65ab0c354c6f6985c2ae70b10b1
Cupid Valentine <westerngod@126.com>
2024-06-02 22:21:35 +0800

    feat: try clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name="rcli", version, author, about, long_about=None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+enum SubCommand {
+    #[command(name = "csv", about = "Show SCV, or convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long)]
+    input: String,
+
+    #[arg(short, long, default_value = "output.json")]
+    output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+
+    #[arg(long, default_value_t = true)]
+    header: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+    print!("{:?}", opts)
 }
